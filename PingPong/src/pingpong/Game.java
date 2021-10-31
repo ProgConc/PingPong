bead113a4186e751773274b69f2981c45ee57478
ProgConc <93448957+ProgConc@users.noreply.github.com>
2021-10-31 21:11:48 +0100

    Version0
    Versión 2

---
+++ PingPong/src/pingpong/Game.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pingpong;
+
+/**
+ *
+ * @author A
+ */
+public class Game {
+
+
+
+    public static final int MAX_TURNS = 10;
+
+
+
+    public static void main(String[] args) {
+
+
+
+        Player player1 = new Player("ping");
+
+        Player player2 = new Player("pong");
+
+
+
+        player1.setNextPlayer(player2);
+
+        player2.setNextPlayer(player1);
+
+
+
+        System.out.println("Game starting...!");
+
+
+
+        player1.play();
+
+
+
+        System.out.println("Game finished!");
+
+    }
+
+}
