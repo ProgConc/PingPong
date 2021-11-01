5127487ebf2067e344225fcc4dcd5cf1380b8cc9
ProgConc <93448957+ProgConc@users.noreply.github.com>
2021-11-01 11:39:35 +0100

    Version espera activa

--- PingPong/src/pingpong/Player.java
+++ PingPong/src/pingpong/Player.java
@@ -10,79 +10,42 @@ package pingpong;
  * @author A
  */
 public class Player implements Runnable {
-
-
-
     private final String text;
-
-
-
+    private final Object lock;
     private Player nextPlayer;
+    private volatile boolean play = false;
 
-
-
-    private volatile boolean mustPlay = false;
-
-
-
-    public Player(String text) {
-
+    public Player(String text,   Object lock) {
         this.text = text;
-
+        this.lock = lock;
     }
-
-
-
+    
     @Override
-
-public void run() {
-
-    while(!Thread.interrupted()) {
-
-        while (!mustPlay);
-
-
-
-        try {
-
-            Thread.sleep(1);
-
-        } catch (InterruptedException e) {
-
-            Thread.currentThread().interrupt();
-
+    public void run() {
+        while(!Thread.interrupted()) {
+            synchronized (lock) {
+                try {
+                    while (!play)
+                        lock.wait();
+                    System.out.println(text);
+                    this.play = false;
+                    nextPlayer.play = true;
+                    lock.notifyAll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
         }
-
-
-
-        System.out.println(text);
-
-
-
-        this.mustPlay = false;
-
-        nextPlayer.mustPlay = true;
-
-
-
     }
-
-}
 
 
 
     public void setNextPlayer(Player nextPlayer) {
-
         this.nextPlayer = nextPlayer;
-
     }
-
-
-
-    public void setMustPlay(boolean mustPlay) {
-
-        this.mustPlay = mustPlay;
-
+    
+    public void setPlay(boolean play) {
+        this.play = play;
     }
 
 }
